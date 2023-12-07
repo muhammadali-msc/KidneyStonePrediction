6aed6b1b09fe49838b2f1c21d84920e2735bd3a8
Muhammad Ali <muhammadalimsc21@gmail.com>
2023-12-08 02:22:11 +0500

    Building CICD Pipeline

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def index():
     
 if __name__ == "__main__":
     
-    app.run(debug = True, host='0.0.0.0', port = 8080)
+    app.run(host='0.0.0.0', port = 8080)
 
     training_pipline = TrainingPipline()
 
